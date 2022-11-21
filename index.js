29a8935d1a48e2c4f22ce0e853aeb69a71934b7f
Abel Mendonça <abel.ncm@gmail.com>
2022-11-21 11:46:17 -0100

    gitignore

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/persons', (req, res) => {
+    
+
+    res.send('Hello persons');
+})
+
+app.get('/say-something', (req, res) => {
+
+    console.log(req.query);
+    const {name, age} = req.query;
+    const myResponse = {
+        name: name,
+        age: age
+    }
+
+    res.send(myResponse);
+})
+
+app.post('/say-something', (req, res) => {
+    console.log(req.body);
+    res.send(`The event name is ${req.body.eventName}`);
+})
+
+app.get('/', (req, res) => {
+    res.send('GET Method called CC')
+})
+
+app.put('/', (req, res) => {
+    res.send('PUT Method called')
+})
+
+app.post('/', (req, res) => {
+    res.send('POST Method called')
+})
+
+app.delete('/', (req, res) => {
+    res.send('DELETE Method called')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
